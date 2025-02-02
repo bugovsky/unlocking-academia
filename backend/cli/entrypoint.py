363525baa38d937564382e815512f3440b1e2bf0
bugovsky <drzhukovskiy@edu.hse.ru>
2025-02-02 07:54:56 +0300

    Poetry CLI (#3)

--- backend/cli/entrypoint.py
+++ backend/cli/entrypoint.py
@@ -9,14 +9,23 @@ backend_app = typer.Typer(short_help="Запуск backend-приложения"
 
 
 @backend_app.command(name="backend", short_help="Запустить fastapi-сервер")
-async def run():
-    async def create_tables():
-        async with DBSettings().setup_db().begin() as conn:
-            # await conn.run_sync(Base.metadata.drop_all)
-            await conn.run_sync(Base.metadata.create_all)
+def launch_backend():
+    async def main():
 
-    await create_tables()
-    uvicorn.run(
-        "backend.api.app:backend_app",
-        **ServerSettings().model_dump(),
-    )
+        async def init_db():
+            # TODO потом удалить, импорт будет не нужен
+            from backend.db import models
+
+            async with DBSettings().engine.begin() as conn:
+                # await conn.run_sync(Base.metadata.drop_all)
+                await conn.run_sync(Base.metadata.create_all)
+
+        await init_db()
+        uvicorn.run(
+            "backend.api.app:backend_app",
+            **ServerSettings().model_dump(),
+        )
+
+    import asyncio
+
+    asyncio.run(main())
