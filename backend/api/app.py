dae587ff6496cd92adc98bc3c1a086f6ef120e3e
d.zhukovskiy <d.zhukovskiy@tbank.ru>
2025-02-22 18:08:49 +0300

    Fix

--- backend/api/app.py
+++ backend/api/app.py
@@ -6,10 +6,10 @@ from backend.api.v1 import user, post, request, comment, rating, auth
 def create_app() -> FastAPI:
     app = FastAPI(title="Unlocking Academia")
     app.include_router(auth.router, prefix="/auth", tags=["Auth"])
-    app.include_router(user.router, prefix="/users", tags=["Users"])
-    app.include_router(post.router, prefix="/posts", tags=["Posts"])
-    app.include_router(request.router, prefix="/requests", tags=["Requests"])
-    app.include_router(comment.router, prefix="/comments", tags=["Comments"])
+    app.include_router(user.router, prefix="/user", tags=["Users"])
+    app.include_router(post.router, prefix="/post", tags=["Posts"])
+    app.include_router(request.router, prefix="/request", tags=["Requests"])
+    app.include_router(comment.router, prefix="/comment", tags=["Comments"])
     app.include_router(rating.router, prefix="/rating", tags=["Rating"])
 
     return app
