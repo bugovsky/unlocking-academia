d91666d1e4d3e753bcad6a5b003f09c0f5679327
d.zhukovskiy <d.zhukovskiy@tbank.ru>
2025-03-17 14:30:09 +0300

    Rm

--- tgbot/handler/callback.py
+++ tgbot/handler/callback.py
@@ -5,32 +5,6 @@ from tgbot.keyboard import get_start_keyboard
 
 router = Router()
 
-# @router.callback_query("q&a_anon")
-# async def qna_anon(callback: types.CallbackQuery):
-#     qa_anon_enabled.add(callback.message.chat.id)
-#     if callback.message.chat.id in qa:
-#         del qa[callback.message.chat.id]
-#     await callback.message.answer("Введите ваш вопрос:")
-#     await callback.answer()
-#
-# @router.callback_query("q&a")
-# async def qna(callback: types.CallbackQuery):
-#     qa[callback.message.chat.id] = []
-#     if callback.message.chat.id in qa_anon_enabled:
-#         qa_anon_enabled.remove(callback.message.chat.id)
-#     await callback.message.answer("Введите ваш вопрос:")
-#     await callback.answer()
-#
-# @router.callback_query("consultation")
-# async def consultation_callback(callback: types.CallbackQuery):
-#     consultation[callback.message.chat.id] = []
-#     if callback.message.chat.id in qa_anon_enabled:
-#         qa_anon_enabled.remove(callback.message.chat.id)
-#     if callback.message.chat.id in qa:
-#         del qa[callback.message.chat.id]
-#     await callback.message.answer(content_manager.get("consult_contact_prompt"))
-#     await callback.answer()
-
 @router.callback_query(F.text.strip() == "back_to_start")
 async def back_to_start(callback: types.CallbackQuery):
     await callback.message.answer(
